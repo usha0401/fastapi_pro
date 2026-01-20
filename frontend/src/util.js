c4dd94b228079c2a0ae15543f99ac044a98c1fdd
usha0401 <vasiusha@gmail.com>
2026-01-20 19:36:49 +0530

    Deploy frontend with production backend URL

--- frontend/src/util.js
+++ frontend/src/util.js
@@ -1,2 +1,5 @@
 // export const API_BASE_URL = "/choreo-apis/fastapi_pro/backend/v1/api"
-export const API_BASE_URL = "http://127.0.0.1:8000/api";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api";
+
+
+export const API_BASE_URL = "https://fastapi-story-backend.onrender.com/api";
